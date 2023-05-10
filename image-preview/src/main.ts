e486f42f2592cd0ba7a0e642deec3c729699f915
chouchouji <70570907+chouchouji@users.noreply.github.com>
2023-05-10 22:39:01 +0800

    Delete image-preview directory

--- image-preview/src/main.ts
+++
@@ -1,6 +0,0 @@
-import './styles/base.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
--- image-preview/src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-// 引入本地图片
-export function getImageUrl(url: string) {
-  return new URL(url, import.meta.url).href
-}
